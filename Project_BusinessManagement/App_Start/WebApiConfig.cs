c3ed7d06550c759c5fdcf5d4e23e5a5e75c3a1e2
cripac <cripac@elempleo.com>
2018-01-05 12:52:11 -0500

    commit para hacer reverso
    ojala

---
+++ Project_BusinessManagement/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Project_BusinessManagement
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "HomeBussiness",
+                routeTemplate: "api/Home/Index/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
