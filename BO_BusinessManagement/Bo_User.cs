b6869babc2a3f585b7ceaaaf7fe0bd257435406c
cripac <cripac@elempleo.com>
2018-01-09 12:30:57 -0500

    commit sobre Usuarios
    Se hace commit sobre los usuarios.

--- BO_BusinessManagement/Bo_User.cs
+++ BO_BusinessManagement/Bo_User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BO_BusinessManagement
 {
-    public class Bo_User
+    public class Bo_User : Bo_Exception
     {
         private int lIdUser;
         private string lFNameUser;
--- Dao_BussinessManagement/Dao_User.cs
+++ Dao_BussinessManagement/Dao_User.cs
@@ -12,5 +12,52 @@ namespace Dao_BussinessManagement
 {
     public class Dao_User
     {
+        List<SqlParameter> lListParam = new List<SqlParameter>();
+
+        public Bo_User Dao_getUserByUser(string pUser, string pPassword)
+        {
+            using (SqlConnection lConex = Dao_UtilsLib.Dao_SqlConnection(lConex))
+            {
+                try
+                {
+                    SqlCommand lCommand = new SqlCommand();
+                    lCommand.CommandText = "spr_GetUserByUser";
+                    lCommand.CommandTimeout = 30;
+                    lCommand.CommandType = CommandType.StoredProcedure;
+                    lCommand.Connection = lConex;
+                    lCommand.Parameters.Add(new SqlParameter("User", pUser));
+                    lCommand.Parameters.Add(new SqlParameter("Password", pPassword));
+
+                    var lReader = lCommand.ExecuteReader();
+                    Bo_User lUser = new Bo_User();
+                    if (lReader.HasRows)
+                    {
+                        while (lReader.Read())
+                        {
+                            lUser.LStatus = new Bo_Status();
+                            lUser.LObject = new Bo_Object();
+                            lUser.LIdUser = Convert.ToInt32(lReader["IdUser"].ToString());
+                            lUser.LUser = lReader["Usuario"].ToString();
+                            lUser.LFNameUser = lReader["FName"].ToString();
+                            lUser.LSNameUser = lReader["SName"].ToString();
+                            lUser.LStatus.LIdStatus = lReader["IdStatus"].ToString();
+                            lUser.LObject.LIdObject = Convert.ToInt32(lReader["IdObject"].ToString());
+                        }
+                    }
+                    Dao_UtilsLib.Dao_CloseSqlconnection(lConex);
+                    return lUser;
+                }
+                catch (Exception e)
+                {
+                    Bo_User lUser = new Bo_User();
+                    lUser.LException = e.Message;
+                    if (e.InnerException != null)
+                        lUser.LInnerException = e.InnerException.ToString();
+                    lUser.LMessageDao = "Hubo un problema en la consulta, contacte al administrador.";
+                    Dao_UtilsLib.Dao_CloseSqlconnection(lConex);
+                    return lUser;
+                }
+            }
+        }
     }
 }
