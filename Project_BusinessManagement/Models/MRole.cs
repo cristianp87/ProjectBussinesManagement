04bc63da42bcd63547494628ba4555b9aef7005a
cristianp87 <cristian.pachon87@hotmail.com>
2018-02-17 20:20:50 -0500

    checkin

--- Project_BusinessManagement/Models/MRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project_BusinessManagement.Models
-{
-    public class MRole
-    {
-        public int LIdRole { get; set; }
-        public int LNameRole { get; set; }
-        public int LFlActive { get; set; }
-    }
-}
